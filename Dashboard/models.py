62e086dab043662f41c532b6cb0d0198953b4628
Kartikeya Dubey <kartikeyadubey2@gmail.com>
2023-10-06 23:19:40 +0530

    Created models

--- Dashboard/models.py
+++ Dashboard/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class animal(models.Model):
+    animal = models.CharField(default="Animal", max_length=255)
+
+    def __str__(self):
+        return self.animal
+
+class sighting(models.Model):
+    latitude = models.FloatField(default=17.543610)
+    longitude = models.FloatField(default=78.574729)
+    animalType = models.ForeignKey(animal, on_delete=models.CASCADE)
+    lastSeen = models.TimeField(auto_now=True)
+
+    
+    def __str__(self):
+        return f""
+
--- Dashboard/views.py
+++ Dashboard/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from Dashboard.models import sighting, animal
 
 # Create your views here.
 def index(request):
+
     return render(request, 'Dashboard/index.html')
