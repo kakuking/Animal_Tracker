fb407e695f3db42716ffc65074af03757a6bb6ee
Kartikeya Dubey <kartikeyadubey2@gmail.com>
2023-10-09 02:02:56 +0530

    Actually shows points from database onto map

--- Dashboard/views.py
+++ Dashboard/views.py
@@ -3,5 +3,6 @@ from Dashboard.models import sighting, animal
 
 # Create your views here.
 def index(request):
-    
-    return render(request, 'Dashboard/index.html', context={'sightings': sighting.objects.all()})
+    all_sightings = sighting.objects.all()
+
+    return render(request, 'Dashboard/index.html', context={'sightings': all_sightings})
