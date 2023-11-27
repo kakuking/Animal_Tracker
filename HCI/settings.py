22bd1aa98cf3d5ac9c22d0277cac4bf7f78d9edd
Kartikeya Dubey <kartikeyadubey2@gmail.com>
2023-11-27 21:49:29 +0530

    changed URL

--- HCI/settings.py
+++ HCI/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 
     'Dashboard'
 ]
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'HCI.urls'
